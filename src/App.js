b3f113eabb5ac7268c2b8a8a1944849ae019c89f
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2025-01-16 13:34:21 +0000

    Add navigation bar with routing functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Container from 'react-bootstrap/Container'
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Salt-Pearl Network Foundation</h1>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.AppHead}>
+            <Switch>
+              <Route 
+                  exact path="/" 
+                  render = {() => <h1>Home Page</h1>}
+              />
+               <Route 
+                  exact path="/aboutus" 
+                  render = {() => <h1>About Page</h1>}  
+              />
+               <Route 
+                  exact path="/contact" 
+                  render = {() => <h1>Contact us Page</h1>} 
+              />
+              <Route 
+                  exact path="/testi" 
+                  render = {() => <h1>Testimonial Page</h1>} 
+              />
+               <Route 
+                  exact path="/support" 
+                  render = {() => <h1>Support Page</h1>}
+              />
+              <Route 
+                  exact path="/privacy" 
+                  render = {() => <h1>Provacy Page</h1>} 
+              />
+          </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import {Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import styles from '../styles/NavBar.module.css'
+import { FaHome } from "react-icons/fa";
+import { IoIosContact } from "react-icons/io";
+import { HiOutlineInformationCircle } from "react-icons/hi";
+import { BsChatQuote } from 'react-icons/bs';
+import { FaDonate } from "react-icons/fa";
+import logo from "../assets/logo.jpg"
+import { NavLink } from 'react-router-dom';
+
+
+const NavBar = () => {
+  return (
+    <div>
+          <Navbar 
+                className={styles.NavBar} 
+                expand="md" 
+                fixed='top'
+            >
+                <Container>
+                <Nav className="text-left">
+                        <div 
+                            className="d-flex align-items-center justify-content-between w-100"
+                        >
+                            <div>
+                                <NavLink to="/" >
+                                    <Navbar.Brand>
+                                        <img 
+                                            src={logo} 
+                                            alt='Brand logo' 
+                                            height='40'
+                                            
+                                        />
+                                    </Navbar.Brand>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </Nav>
+                    <Navbar.Toggle 
+                        aria-controls="basic-navbar-nav" 
+                        className={styles.ToggleButton}
+                    />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto text-left">
+                            <NavLink 
+                                exact to = "/" 
+                                className={`${styles.NavLink} mr-2 ${styles.HomeIcon}`} 
+                                activeClassName = {styles.Active} 
+                            >
+                                <div className={`${styles.LinkContainer} ${styles.AddandSign}`}>
+                                    <FaHome className='mr-1' size={30}/>
+                                    Home
+                                </div>
+                            </NavLink>   
+                            <NavLink 
+                                exact to = "/aboutus" 
+                                className={`${styles.NavLink} mr-2 ${styles.HomeIcon}`} 
+                                activeClassName = {styles.Active} 
+                            >
+                                <div className={`${styles.LinkContainer} ${styles.AddandSign}`}>
+                                    <HiOutlineInformationCircle className='mr-1' size={30}/>
+                                    About Us
+                                </div>
+                            </NavLink>            
+                        
+                        <NavLink 
+                                exact to = "contact" 
+                                className={`${styles.NavLink} mr-2 ${styles.HomeIcon}`} 
+                                activeClassName = {styles.Active} 
+                            >
+                                <div className={`${styles.LinkContainer} ${styles.AddandSign}`}>
+                                    <IoIosContact className='mr-1' size={30}/>
+                                    Contact
+                                </div>
+                        </NavLink>
+                        <NavLink 
+                                exact to = "testi" 
+                                className={`${styles.NavLink} mr-2 ${styles.HomeIcon}`} 
+                                activeClassName = {styles.Active} 
+                            >
+                                <div className={`${styles.LinkContainer} ${styles.AddandSign}`}>
+                                    <BsChatQuote className='mr-1' size={30}/>
+                                    Testimonials
+                                </div>
+                        </NavLink> 
+                        <NavLink 
+                                exact to = "support" 
+                                className={`${styles.NavLink} mr-2 ${styles.HomeIcon}`} 
+                                activeClassName = {styles.Active} 
+                            >
+                                <div className={`${styles.LinkContainer} ${styles.AddandSign}`}>
+                                    <FaDonate className='mr-1' size={30}/>
+                                    Support
+                                </div>
+                        </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+
+            </Navbar>
+    </div>
+  )
+}
+
+export default NavBar
