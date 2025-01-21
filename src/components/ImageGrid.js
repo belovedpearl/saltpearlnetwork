47cc587ffd46dad4f17c0dfb9e6c06e1db8b6f18
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2025-01-21 12:50:44 +0000

    Add required image to homepage

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import styles from '../styles/ImageGrid.module.css';
-import image1 from '../assets/img1.jpg';
-import image2 from '../assets/img2.jpg';
-import image3 from '../assets/img3.jpg';
-// import image4 from '../assets/image4.jpg'
+import image1 from '../assets/sp1.jpg';
+import image2 from '../assets/sp3.jpg';
+import image3 from '../assets/sp8.jpg';
+import image4 from '../assets/sp6.jpg'
 
 const ImageGrid = () => {
-  const images = [
-    image1,
-    image2,
-    image3
-    // image4
-  ];
+    const images = [
+      image1,
+      image2,
+      image4,
+      image3 
+    ];
 
-  return (
-    <div className={styles.flexContainer}>
-        {images.map((image, index) => (
-        <div key={index} className={styles.flexItem}>
-            <img src={image} alt={`Gallery display ${index + 1}`} className={styles.image} />
+    return (
+        <div className={styles.flexContainer}>
+            {images.map((image, index) => (
+            <div key={index} className={styles.flexItem}>
+                <img 
+                  src={image} 
+                  alt={`Gallery display ${index + 1}`} 
+                  className={styles.image} 
+                />
+            </div>
+            ))}
         </div>
-        ))}
-    </div>
-  );
+    );
 };
 
 export default ImageGrid;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,51 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../styles/HomePage.module.css'
 import GetInvolvedDropdown from '../components/GetInvolvedDropdown';
-import image1 from '../assets/img1.jpg';
-import image2 from '../assets/img2.jpg';
-import image3 from '../assets/img3.jpg';
+import image1 from '../assets/sp2.jpg';
+import image4 from '../assets/sp4.jpg';
+import image2 from '../assets/sp9.jpg';
+import image3 from '../assets/sp10.jpg';
 import ImageGrid from '../components/ImageGrid';
 
 
 const HomePage = () => {
 
-  const [currentImage, setCurrentImage] = useState(0);
-  const images = [
-    image1,
-    image2,
-    image3,
-  ];
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [
+      image1,
+      image2,
+      image4,
+      image3,
+    ];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 3000); // Change image every 3 seconds
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCurrentImage((prev) => (prev + 1) % images.length);
+      }, 3000); // Change image every 3 seconds
 
     return () => clearInterval(interval); // Cleanup on unmount
-  }, [images.length]);
+    }, [images.length]);
     return (
-      <div>
-      <div className={styles.container}> 
-        <div className={styles.leftbox}>
-            <h1>Building the young for a better world</h1>
-            <p>
-                Join us in reaching out to children and teens, sharing hope and love
-                to everychild.
-            </p>
-            <GetInvolvedDropdown />
+        <div>
+          <div className={styles.container}> 
+              <div className={styles.leftbox}>
+                  <h1>Building the young for a better world</h1>
+                  <p>
+                      Join us in reaching out to children and teens, sharing hope and love
+                      to everychild.
+                  </p>
+                  <GetInvolvedDropdown />
+              </div>
+              <div className={styles.rightbox}>
+                <img 
+                  src={images[currentImage]} 
+                  alt="Carousel" 
+                />
+            </div>
+          </div>
+          <ImageGrid />
         </div>
-        <div className={styles.rightbox}>
-          <img 
-            src={images[currentImage]} 
-            alt="Carousel" 
-          />
-        </div>
-      </div>
-      
-      <ImageGrid />
-      
-      </div>
     );
 }
-
 export default HomePage
