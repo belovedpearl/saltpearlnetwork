3532ed48c717c66c0a1721654b4d30c2d5703264
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2025-06-26 11:25:18 +0000

    Modify support button

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 import styles from '../styles/DonateButton.module.css'
 
 export default function DonateButton() {
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        // Reset loading when the component is mounted
+        setLoading(false);
+    }, []);
+
     const handleClick = () => {
         setLoading(true);
         setTimeout(() => {
         window.open("https://sandbox.flutterwave.com/donate/5uaf6a0sdxpe", "_blank");
+        setLoading(false);
         }, 1000);
     };
 
